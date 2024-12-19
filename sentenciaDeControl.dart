25eb372342147b2ec1554797297e77cedf035cd1
JHOSEPH <jhoseph@localhost.localdomain>
2024-12-19 16:18:44 -0400

    add: sentencia y control

---
+++ sentenciaDeControl.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Sentencia de control y flujo
+  String dia = 'Lunes';
+
+  if (dia == 'Martes') {
+    print('el dia es %{dia}');
+  } else {
+    print('es otro dia y no es el dia ${dia}');
+  }
+}
